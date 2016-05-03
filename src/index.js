56187d0bc1aa6bdcd7dd9222710e113d0c61e70e
Chuck Conway <charles@cconway.com>
2016-05-02 21:58:23 -0700

    completed the React app

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import SearchBar from './components/search_bar';
@@ -15,21 +16,27 @@ class App extends Component {
 
       this.state = { videos: [], selectedVideo:null};
 
-      YTSearch({key:API_KEY, term:'surfboards'}, (videos)=> {
+      this.videoSearch('surfboards');
 
-        this.setState({
-          videos,
-          selectedVideo:videos[0]
-        });
-
-      });
   }
 
+  videoSearch(term){
+    YTSearch({key:API_KEY, term:term}, (videos)=> {
+      this.setState({
+        videos,
+        selectedVideo:videos[0]
+      });
+
+    });
+  }
 
   render(){
+
+    const videoSearch = _.debounce((term) => {this.videoSearch(term)}, 300);
+
     return (
       <div>
-        <SearchBar />
+        <SearchBar onSearchTermChange={videoSearch} />
         <VideoDetail video={this.state.selectedVideo} />
         <VideoList
           onVideoSelect={selectedVideo => this.setState({selectedVideo})}
